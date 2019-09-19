b979e1fde2b0d928ca314f37be1dc8607117c959
JiwoonKim <diana0501@naver.com>
2019-09-19 17:38:06 +0900

    [카카오 블라인드 2018 기출] #7 블록게임 문제 추가

---
+++ 2018_kakao_blind_coding_test/7_block_game.cpp
@@ -0,0 +1,21 @@
+/**
+ *  [7] 블록게임
+ *      - 보드 위에 놓인 블록의 상태가 담긴 2차원 배열 board가 주어질 때, 
+ *        검은 블록을 떨어뜨려 없앨 수 있는 블록 개수의 최댓값을 구하라.
+ *        (각 블록은 )
+ *      - Reference: https://programmers.co.kr/learn/courses/30/lessons/42893
+ * 
+ * 
+ *  * Solution
+ * 
+ * 
+ *  ** What I learned
+ * 
+ * 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ *  
+ */
